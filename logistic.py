474e35941a0b4060a2cf9cee183ea4f0e5c81a5d
Athena Hernandez <athena.hernandez.23@bishops.com>
2022-11-15 23:57:19 -0800

    Randomizes dataset and resets index

--- logistic.py
+++ logistic.py
@@ -28,46 +28,12 @@ def main():
         df_subset_1 = df_subset_1.head(len(df_subset_0))
 
     df = pd.concat([df_subset_0, df_subset_1])
+    df = df.sample(frac=1, random_state=1).reset_index()
     print(df.head(), len(df))
+
+    # Split data into test and training
+    # Perform what type of classification
+    # Graph ? Check Dr. J's GitHub
+
 if __name__ == '__main__':
     main()
-
-# rows_used = int(len(complete_df.index) * .1)
-
-# # df.head()
-# print(complete_df)
-
-# rows_used = int(len(complete_df.index) * .1)
-
-# df = complete_df.loc[0:rows_used][:]
-
-# df['Type'] == 1
-# # df = df[(df.index > np.percentile(df.index, 10)) & (df.index <= np.percentile(df.index, 10))]
-# print(df)
-
-# df.loc[(df['Type'] == 0) & (df['col2'] < value)]
-# create 0 df, grab 10%; create 1 df, grab 10%; when creating test df training will shuffle randomize
-
-
-## Leave out first 10% or 90% for both benign and malicious
-
-# digits = load_digits()
-# digitsX = digits.images.reshape(len(digits.images), 64)
-# digitsY = digits.target
-# trainX, testX, trainY, testY = train_test_split(
-#     digitsX, digitsY, test_size = 0.3, shuffle = True
-#     )
-
-# classifier = LogisticRegression(max_iter = 10000)
-# classifier.fit(trainX, trainY)
-# preds = classifier.predict(testX)
-
-# correct = 0
-# incorrect = 0
-# for pred, gt in zip(preds, testY):
-#     if pred == gt: correct += 1
-#     else: incorrect += 1
-# print(f"Correct: {correct}, Incorrect: {incorrect}, % Correct: {correct/(correct + incorrect): 5.2}")
-
-# plot_confusion_matrix(classifier, testX, testY)
-# pyplot.show()
